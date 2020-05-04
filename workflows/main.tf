79daea24faad940d60dc6adf572ebce1f4db3854
Lindsley, Chris <lindsleycj@ornl.gov>
2020-05-04 09:44:19 -0400

    change default CMA version, update workflow.zip version

--- workflows/main.tf
+++ workflows/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 module "acme_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v1.17.0/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v1.18.0/terraform-aws-cumulus-workflow.zip"
 
   prefix                   = local.prefix
   name                     = "ACMEWorkflow"
