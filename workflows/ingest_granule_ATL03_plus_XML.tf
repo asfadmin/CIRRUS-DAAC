96cb0533769554db3471b1735659da104f0106b0
MARK SCHWAB <32745414+schwabm@users.noreply.github.com>
2020-05-21 14:53:46 -0600

    Update ingest_granule_ATL03_plus_XML.tf

--- workflows/ingest_granule_ATL03_plus_XML.tf
+++ workflows/ingest_granule_ATL03_plus_XML.tf
@@ -1,23 +1,3 @@
-resource "aws_lambda_function" "atl03_extract_browse" {
-  function_name    = "${local.prefix}-browse-imagery-atl03"
-
-  filename         = "${path.module}/lambdas/lambda-browse-imagery-from-hdf5/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambdas/lambda-browse-imagery-from-hdf5/lambda.zip")
-
-  # source = "https://github.com/nsidc/lambda-browse-imagery-from-hdf5/releases/download/v0.1.1/lambda-hdf-brw.zip"
-
-  handler          = "ingest_granule_ATL03.lambda_handler"
-  role             = data.terraform_remote_state.cumulus.outputs.lambda_processing_role_arn
-  runtime          = "python3.6"
-  timeout          = 60
-
-  layers = [aws_lambda_layer_version.lambda_dependencies.arn]
-
-  // vpc_config {
-  //   subnet_ids         = var.subnet_ids
-  //   security_group_ids = [aws_security_group.no_ingress_all_egress.id]
-  // }
-}
 
 
 // Add XMLTransform - Transform ISO XML to CMR ISO XML
@@ -39,22 +19,7 @@ resource "aws_lambda_function" "XMLTransform" {
 }
 
 
-module "discover_granules_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v1.22.1/terraform-aws-cumulus-workflow.zip"
-
-  prefix          = local.prefix
-  name            = "DiscoverGranules"
-  workflow_config = data.terraform_remote_state.cumulus.outputs.workflow_config
-  system_bucket   = local.system_bucket
-  // tags            = local.tags
-
-  state_machine_definition = templatefile("./disco_grans.json", {
-    disco_task_arn = data.terraform_remote_state.cumulus.outputs.discover_granules_task.task_arn
-    queue_task_arn = data.terraform_remote_state.cumulus.outputs.queue_granules_task.task_arn
-  })
-}
-
-module "ingest_atl03_granule_with_browse_workflow" {
+module "ingest_atl03_granule_with_browse_and_xml_workflow" {
   source = "https://github.com/nasa/cumulus/releases/download/v1.22.1/terraform-aws-cumulus-workflow.zip"
 
   prefix          = local.prefix
