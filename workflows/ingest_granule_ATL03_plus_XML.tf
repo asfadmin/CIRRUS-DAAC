5b195547e98ee6d6ffdc1c624bd37d32814b3a7c
MARK SCHWAB <32745414+schwabm@users.noreply.github.com>
2020-05-22 09:22:06 -0600

    remove files to grans task dup

--- workflows/ingest_granule_ATL03_plus_XML.tf
+++ workflows/ingest_granule_ATL03_plus_XML.tf
@@ -33,7 +33,6 @@ module "ingest_atl03_granule_with_browse_and_xml_workflow" {
     processing_browse_task_arn = aws_lambda_function.atl03_extract_browse.arn
     files_to_grans_task_arn = data.terraform_remote_state.cumulus.outputs.files_to_granules_task.task_arn
     processing_xml_task_arn = aws_lambda_function.XMLTransform.arn
-    files_to_grans_task_arn = data.terraform_remote_state.cumulus.outputs.files_to_granules_task.task_arn
     move_task_arn = data.terraform_remote_state.cumulus.outputs.move_granules_task.task_arn
   })
 }
