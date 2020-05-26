750becf383e1c65909e312ce484ca03389c6c7bb
Mark Schwab <schwabm@nsidc.org>
2020-05-26 09:45:34 -0600

    removed movegrans step to test

--- workflows/ingest_granule_ATL03_plus_XML.tf
+++ workflows/ingest_granule_ATL03_plus_XML.tf
@@ -34,6 +34,5 @@ module "ingest_atl03_granule_with_browse_and_xml_workflow" {
     files_to_grans_task_arn = data.terraform_remote_state.cumulus.outputs.files_to_granules_task.task_arn
     processing_xml_task_arn = aws_lambda_function.XMLTransform.arn
     files_to_grans_task2_arn = data.terraform_remote_state.cumulus.outputs.files_to_granules_task.task_arn
-    move_task_arn = data.terraform_remote_state.cumulus.outputs.move_granules_task.task_arn
   })
 }
