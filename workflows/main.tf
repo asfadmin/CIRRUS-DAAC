5aa0dd5bc6bd7c54198c5269076f3028484b8c6d
Lindsley, Chris <lindsleycj@ornl.gov>
2020-08-18 12:12:10 -0400

    upgrade Cumulus to v2.0.2

--- workflows/main.tf
+++ workflows/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 module "acme_workflow" {
-  source = "https://github.com/nasa/cumulus/releases/download/v2.0.1/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v2.0.2/terraform-aws-cumulus-workflow.zip"
 
   prefix                   = local.prefix
   name                     = "ACMEWorkflow"
