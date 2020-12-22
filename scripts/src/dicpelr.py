2c32937d54e8951f1d3445bed59ff6ff1655c03b
McKade Sorensen <dmsorensen@alaska.edu>
2020-12-22 11:01:38 -0900

    Updated function names and added more ARGS

--- scripts/src/dicpelr.py
+++ scripts/src/dicpelr.py
@@ -10,12 +10,9 @@ from subprocess import call
 
 import boto3
 
-# TODO: Fix 2020-12-03 23:05:31,272 - Could not execute AWS request: An error occurred
-#   (ClusterContainsContainerInstancesException) when calling the DeleteCluster operation:
-#   The Cluster cannot be deleted while Container Instances are active or draining.
-
 ARGS = "-d cloudwatch -d ecs -d es -d events -d lambda -d logs -d " \
-       "secretsmanager -d sns -d sqs -d states -d s3"
+       "secretsmanager -d sns -d sqs -d states -d s3 -d cloudformation" \
+       " -d dynamodb -d ec2"
 
 EVENT_SOURCE_MAPPING_FILE = "event_source_mapping.json"
 BUCKET_FILE = "bucket.txt"
@@ -96,15 +93,17 @@ class Dicpelr:
     #   *********************************************************
     #  ******* Start of Functions for Deleting Resources *******
     # *********************************************************
-    def delete_all_resources(self):
+    def delete_all_persistent_resources(self):
         self.delete_iam_roles()
         self.delete_lambda_event_source_mappings()
         self.delete_s3()
+        self.delete_ecs_cluster_profile()
+        self._run_purge_script(ARGS)
 
     def _run_purge_script(self, args):
         arguments = f"{self.AWS_CLI_PRAMS} {args}"
-        if self.no_dry_run:
-            arguments = f"  --no-dry-run -c false {arguments}"
+        # TODO: Add an if statement for no dry run.
+        arguments = f" --no-dry-run -c false {arguments}"
         call(f"python3 purge.py {self.AWS_CLI_PRAMS} {arguments}".split())
 
     def _delete_iam_rule_attached_policy(self, iam_role_name):
@@ -116,11 +115,8 @@ class Dicpelr:
                       f"{iam_role_name} --policy-name {policy}")
 
     def delete_iam_roles(self):
-        # TODO: Update this to do a similar process to delete_lambda_event_source_mappings()
-        # TODO: Fix "An error occurred (DeleteConflict) when calling the DeleteRole operation: Cannot delete entity,
-        #  must delete policies first."
         for role in self.iam_roles:
-            REGEX = re.compile(f"{self.STACK_NAME}(-|_)(.*)")
+            REGEX = re.compile(f"{self.STACK_NAME}([-_])(.*)")
             role_name = role["RoleName"]
             if re.match(REGEX, role_name):
                 self._delete_iam_rule_attached_policy(role_name)
@@ -137,18 +133,6 @@ class Dicpelr:
                 print(f"Deleting {esa} with the UUID: {uuid}")
                 os.system(f"aws lambda --profile {self.AWS_PROFILE} delete-event-source-mapping --uuid {uuid}")
 
-    def delete_tea_cloudformation(self):
-        CLOUDFORMATION_ARGS = "-d cloudformation"
-        self._run_purge_script(CLOUDFORMATION_ARGS)
-
-    def delete_dynamodb_tables(self):
-        DYNAMO_DB_ARGS = "-d dynamodb"
-        self._run_purge_script(DYNAMO_DB_ARGS)
-
-    def delete_ec2(self):
-        EC2_ARGS = "-d ec2"
-        self._run_purge_script(EC2_ARGS)
-
     def delete_s3(self):
         for b in self.s3_bucket_names:
             print(f"Deleting the s3 bucket: {b}")
@@ -164,5 +148,5 @@ class Dicpelr:
 
 if __name__ == "__main__":
     d = Dicpelr()
-    d.delete_all_resources()
+    d.delete_all_persistent_resources()
 
