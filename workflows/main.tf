cce3f2462fe033b887ae9ddda09c3763f8b876e1
Lindsley, Chris <lindsleycj@ornl.gov>
2021-05-06 17:44:56 -0400

    updates for Cumulus v8.1.0

--- workflows/main.tf
+++ workflows/main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 
 module "acme_workflow" {
 
-  source = "https://github.com/nasa/cumulus/releases/download/v6.0.0/terraform-aws-cumulus-workflow.zip"
+  source = "https://github.com/nasa/cumulus/releases/download/v8.1.0/terraform-aws-cumulus-workflow.zip"
 
   prefix          = local.prefix
   name            = "ACMEWorkflow"
