8ef0467a1c32e339d650ab9b3c5cd7e6365dd209
Lindsley, Chris <lindsleycj@ornl.gov>
2023-05-03 14:52:36 -0400

    fix tflint issue

--- dashboard/main.tf
+++ dashboard/main.tf
@@ -1,13 +1,22 @@
 terraform {
   required_providers {
-    version = ">= 3.75.2"
-    null = "~> 2.1.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.75.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
   }
   backend "s3" {
   }
 }
 
 provider "aws" {
+  ignore_tags {
+    key_prefixes = ["gsfc-ngap"]
+  }
 }
 
 locals {
