c30a9c0f31fa5714a4704b42b33852b6f3f28fa8
Rohan Weeden <reweeden@alaska.edu>
2024-05-20 11:10:58 -0800

    Refactor bucket config to use a single map

--- daac/locals.tf
+++ daac/locals.tf
@@ -13,17 +13,36 @@ locals {
     DAR = "NO"
   }
 
+  # Merge the bucket_config with base
+  # First, merge any configs that would override the base
+  partial_bucket_config = {
+    for n, cfg in var.bucket_config_base : n => merge(cfg, lookup(var.bucket_config, n, cfg))
+  }
+  # Then combine the override map with the merged base to capture additions
+  bucket_config = merge(var.bucket_config, local.partial_bucket_config)
 
-  standard_bucket_names  = [for n in var.standard_bucket_names : "${local.prefix}-${n}"]
-  protected_bucket_names = [for n in var.protected_bucket_names : "${local.prefix}-${n}"]
-  public_bucket_names    = [for n in var.public_bucket_names : "${local.prefix}-${n}"]
-  workflow_bucket_names  = [for n in var.workflow_bucket_names : "${local.prefix}-${n}"]
+  # Bucket types. These lists should not overlap
+  standard_bucket_names  = [for n, cfg in local.bucket_config : "${local.prefix}-${n}" if cfg.type == "standard"]
+  protected_bucket_names = [for n, cfg in local.bucket_config : "${local.prefix}-${n}" if cfg.type == "protected"]
+  public_bucket_names    = [for n, cfg in local.bucket_config : "${local.prefix}-${n}" if cfg.type == "public"]
+  workflow_bucket_names  = [for n, cfg in local.bucket_config : "${local.prefix}-${n}" if cfg.type == "workflow"]
 
-  standard_bucket_map  = { for n in var.standard_bucket_names : n => { name = "${local.prefix}-${n}", type = n } }
-  protected_bucket_map = { for n in var.protected_bucket_names : n => { name = "${local.prefix}-${n}", type = "protected" } }
-  public_bucket_map    = { for n in var.public_bucket_names : n => { name = "${local.prefix}-${n}", type = "public" } }
-  workflow_bucket_map  = { for n in var.workflow_bucket_names : n => { name = "${local.prefix}-${n}", type = "workflow" } }
-  partner_bucket_map   = { for n in var.partner_bucket_names : n => { name = n, type = "partner" } }
+  # Bucket attributes. These will enable additional configuration on some bucket
+  # in one of the lists above.
+  distribution_bucket_oais = { for n, cfg in local.bucket_config : n => cfg.oai if cfg.oai != null }
+
+  base_bucket_map = {
+    for n, cfg in local.bucket_config : n => {
+      name = "${local.prefix}-${n}"
+      type = cfg.type == "standard" ? n : cfg.type
+    }
+  }
+  partner_bucket_map = {
+    for n in var.partner_bucket_names : n => {
+      name = n
+      type = "partner"
+    }
+  }
   internal_bucket_map = {
     internal = {
       name = "${local.prefix}-internal"
@@ -33,11 +52,8 @@ locals {
 
   # creates a TEA style bucket map, is outputted via outputs.tf
   bucket_map = merge(
-    local.standard_bucket_map,
+    local.base_bucket_map,
     local.internal_bucket_map,
-    local.protected_bucket_map,
-    local.public_bucket_map,
-    local.workflow_bucket_map,
-    local.partner_bucket_map
+    local.partner_bucket_map,
   )
 }
