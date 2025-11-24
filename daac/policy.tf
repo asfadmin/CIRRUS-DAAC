1cc7f9f19bb0867e8645d82f0c7c46aa859df7f5
etcart <37375117+etcart@users.noreply.github.com>
2025-11-24 12:15:30 -0500

    Update daac/policy.tf
    Co-authored-by: Matt Perry <mperry37@alaska.edu>

--- daac/policy.tf
+++ daac/policy.tf
@@ -226,7 +226,7 @@ resource "aws_s3_bucket_policy" "workflow_bucket_cross_acoount_access" {
       },
       {
         Sid = "CrossAccountReadAccessEcs"
-        Effect = "allow"
+        Effect = "Allow"
         Principal = {
           AWS = "arn:aws:iam::${var.consolidation_acct_id}:role/${var.consolidation_deploy_name}-cumulus-${local.consolidation_maturity}-lambda-processing"
         }
